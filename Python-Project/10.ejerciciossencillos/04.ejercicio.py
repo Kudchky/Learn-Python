ffdf48d2a19c48a5fe7d4c77f5381b847659f412
Pol Castillo <rpcastilloy@gmail.com>
2025-05-08 22:24:18 -0500

    Inciando ejercicios con for, subiendo nivel

---
+++ Python-Project/10.ejerciciossencillos/04.ejercicio.py
@@ -0,0 +1,19 @@
+print("\nBienvenido a App Tabla de Multiplicar"
+      "--------------------------------------\n")
+while True:
+    try:
+        numero_input = int(input("Ingrese un numero: ").strip())
+        if numero_input <= 0:
+            print("Error: Ingrese numeros enteros positivos mayor a 0, intente de nuevo\n")
+            continue
+        break
+    except ValueError:
+        print("Error: Ingrese valores numericos, intente de nuevo\n")
+
+print(f"\nSe generó la tabla de multiplicar de {numero_input}")
+print("---------------------------------------\n")
+
+for numero in range(13):
+    numero_multiplicado = numero_input * numero
+    if numero_multiplicado % 2 == 0 and numero > 0:
+        print(f"  {numero_input} x {numero} = {numero_multiplicado}")
