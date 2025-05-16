76bb56329413838f5f3ac361e70540040b767d52
Pol Castillo <rpcastilloy@gmail.com>
2025-05-16 11:36:32 -0500

    Add object snake

--- Python-Project/12.navegandomapa/01.ejercicio.py
+++ Python-Project/12.navegandomapa/01.ejercicio.py
@@ -9,8 +9,25 @@ MAP_WIDTH = 20
 MAP_HEIGHT = 15
 NUM_OBJECTS = 10
 
+tail_length = 0
+tail = []
 my_position = [3, 1]
 map_objects = []
+
+def centered_message(message):
+    screen_width = MAP_WIDTH * 3
+    padding = (screen_width - len(message)) // 2
+    print("\n" * (MAP_HEIGHT // 2))
+    print(" " * padding + message)
+    print("\n" * (MAP_HEIGHT // 2))
+
+def add_object():
+    while True:
+        obj_x = random.randint(0, MAP_WIDTH - 1)
+        obj_y = random.randint(0, MAP_HEIGHT - 1)
+        if [obj_x, obj_y] not in map_objects and [obj_x, obj_y] != my_position and [obj_x, obj_y] not in tail:
+            map_objects.append([obj_x, obj_y])
+            break
 
 while len(map_objects) < NUM_OBJECTS:
     x = random.randint(0, MAP_WIDTH - 1)
@@ -20,6 +37,7 @@ while len(map_objects) < NUM_OBJECTS:
         map_objects.append([x, y])
 
 while True:
+    lose = False
     print("+" + "-" * MAP_WIDTH * 3 + "+")
     for y in range(MAP_HEIGHT):
         print("|", end="")
@@ -28,12 +46,17 @@ while True:
             # Player in the position
             if [x, y] == my_position:
                 print(" @ ", end="")
-                # If there is an object in the position of the player
+                # If there is an object in the position of the player.
                 if [x, y] in map_objects:
                     map_objects.remove([x, y])
+                    tail_length += 1
+                    add_object()
             # If only is an object
             elif [x, y] in map_objects:
                 print(" * ", end="")
+            # If the position is part of the tail.
+            elif [x, y] in tail:
+                print(" o ", end="")
             # Empty space
             else:
                 print("   ", end="")
@@ -44,26 +67,51 @@ while True:
     direction = readchar.readchar()
 
     if direction == "w":
+        tail.insert(0, my_position.copy())
         my_position[POS_Y] -= 1
         my_position[POS_Y] %= MAP_HEIGHT
-        #if my_position[POS_Y] < 0:
-        #    my_position[POS_Y] = MAP_HEIGHT - 1
+        if len(tail) > tail_length:
+            tail = tail[:tail_length]
+        if my_position in tail:
+            os.system("clear")
+            centered_message("💀 GAME OVER 💀")
+            time.sleep(2)
+            break
     elif direction == "s":
+        tail.insert(0, my_position.copy())
         my_position[POS_Y] += 1
         my_position[POS_Y] %= MAP_HEIGHT
-        #if my_position[POS_Y] > MAP_HEIGHT - 1:
-        #    my_position[POS_Y] = 0
+        if len(tail) > tail_length:
+            tail = tail[:tail_length]
+        if my_position in tail:
+            os.system("clear")
+            centered_message("💀 GAME OVER 💀")
+            time.sleep(2)
+            break
     elif direction == "a":
+        tail.insert(0, my_position.copy())
         my_position[POS_X] -= 1
         my_position[POS_X] %= MAP_WIDTH
-        #if my_position[POS_X] < 0:
-        #    my_position[POS_X] = MAP_WIDTH - 1
+        if len(tail) > tail_length:
+            tail = tail[:tail_length]
+        if my_position in tail:
+            os.system("clear")
+            centered_message("💀 GAME OVER 💀")
+            time.sleep(2)
+            break
     elif direction == "d":
+        tail.insert(0, my_position.copy())
         my_position[POS_X] += 1
         my_position[POS_X] %= MAP_WIDTH
-        #if my_position[POS_X] > MAP_WIDTH - 1:
-        #    my_position[POS_X] = 0
+        if len(tail) > tail_length:
+            tail = tail[:tail_length]
+        if my_position in tail:
+            os.system("clear")
+            centered_message("💀 GAME OVER 💀")
+            time.sleep(2)
+            break
     elif direction == "q":
+        print("You left the game...!")
         break
     else:
         print("Enter a valid value: [W, S, A, D]")
