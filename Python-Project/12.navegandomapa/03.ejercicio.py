318b44a0842a53963d880d29ddec6dc68dc1774d
Pol Castillo <rpcastilloy@gmail.com>
2025-05-29 02:18:47 -0500

    Update project, add tail snake done

--- Python-Project/12.navegandomapa/03.ejercicio.py
+++ Python-Project/12.navegandomapa/03.ejercicio.py
@@ -9,32 +9,55 @@ WIDTH = 20
 HEIGHT = 15
 MAX_FOOD = 10
 LIST_FOOD = []
+TRACE_SNAKE = []
+TAIL_SNAKE = []
+count_food = 0
 
 #Funtions----
 def add_food():
     while len(LIST_FOOD) < MAX_FOOD:
         food_x = random.randint(0, WIDTH - 1)
         food_y = random.randint(0, HEIGHT - 1)
-        if [food_x, food_y] != my_position:
+        if ([food_x, food_y] != my_position and
+            [food_x, food_y] not in LIST_FOOD and
+            [food_x, food_y] not in TAIL_SNAKE
+        ):
             LIST_FOOD.append([food_x, food_y])
 
 def move_snake(position):
+    global TAIL_SNAKE
     print("Press a key (w, s, a, d) or q to exit...!")
     key = readchar.readchar().upper()
     match key:
         case "W":
+            # Define trace snake
+            TRACE_SNAKE.insert(0, position)
+            # Define tail snake
+            if count_food > 0:
+                TAIL_SNAKE = TRACE_SNAKE[: count_food]
+            # Define position new
             position = [position[0], position[1] - 1]
+            # Define that snake appears to the other side
             if position[1] == -1:
                 position = [position[0], HEIGHT - 1]
         case "S":
+            TRACE_SNAKE.insert(0, position)
+            if count_food > 0:
+                TAIL_SNAKE = TRACE_SNAKE[: count_food]
             position = [position[0], position[1] + 1]
             if position[1] == HEIGHT:
                 position = [position[0], 0]
         case "A":
+            TRACE_SNAKE.insert(0, position)
+            if count_food > 0:
+                TAIL_SNAKE = TRACE_SNAKE[: count_food]
             position = [position[0] - 1, position[1]]
             if position[0] == -1:
                 position = [WIDTH - 1, position[1]]
         case "D":
+            TRACE_SNAKE.insert(0, position)
+            if count_food > 0:
+                TAIL_SNAKE = TRACE_SNAKE[: count_food]
             position = [position[0] + 1, position[1]]
             if position[0] == WIDTH:
                 position = [0, position[1]]
@@ -61,6 +84,10 @@ while True:
                     print(" @ ", end="")
                     if [x, y] in LIST_FOOD:
                         LIST_FOOD.remove([x, y])
+                        count_food += 1
+                        add_food()
+                elif [x, y] in TAIL_SNAKE:
+                    print(" o ", end="")
                 elif [x, y] in LIST_FOOD:
                     print(" ■ ", end="")
                 else:
