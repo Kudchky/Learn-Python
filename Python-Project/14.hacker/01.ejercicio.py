7dfaf2fbd71773941a2e9d798749084e35b1699c
Pol Castillo <rpcastilloy@gmail.com>
2025-06-11 19:58:32 -0500

    finish project hacker

--- Python-Project/14.hacker/01.ejercicio.py
+++ Python-Project/14.hacker/01.ejercicio.py
@@ -7,7 +7,6 @@ from time import sleep
 
 FILE_NAME = "for_you.txt"
 
-
 def delay_action():
     n_hour = randrange(1, 3)
     n_minutes = randrange(0, 60)
@@ -40,37 +39,35 @@ def get_edge_history(user_path):
         try:
             with sqlite3.connect(history_path) as connection:
                 cursor = connection.cursor()
-            cursor.execute("""
+                cursor.execute("""
                     SELECT title, last_visit_time, url
                     FROM urls 
                     ORDER BY last_visit_time DESC
                     LIMIT 5
                     """)
-            urls = cursor.fetchall()
+                urls = cursor.fetchall()
 
-            print("\nLast sites visited in Edge: ")
-            for title, visit_time, url in urls:
-                readable_time = convert_edge_time(visit_time)
-                print(f"- {title or 'No title'}\n {readable_time}\n {url}\n")
-            break
+            return urls
         except sqlite3.Error as e:
             print(f"Error accessing Edge: {e}")
             print("Re-accessing in 30 seconds...\n")
-            sleep(30)
+            sleep(5)
 
-def hacker_desktop(desktop_path):
-
+def hacker_desktop(desktop_path, urls):
     with open(desktop_path / FILE_NAME, "w") as hacker_file:
         hacker_file.write("hi, I am hacker and I have infiltrated your system")
-
+        print("\nLast sites visited in Edge: ")
+        for title, visit_time, url in urls:
+            readable_time = convert_edge_time(visit_time)
+            hacker_file.write(f"- {title or 'No title'}\n {readable_time}\n {url}\n")
 
 def main():
     delay_action()
     user_path, desktop_path = get_user_desktop()
     if desktop_path is None:
         exit("The desktop is not found. Finishing the program")
-    hacker_desktop(desktop_path)
-    get_edge_history(user_path)
+    urls = get_edge_history(user_path)
+    hacker_desktop(desktop_path, urls)
 
 
 if __name__ == "__main__":
